545be6f1630ecd7e19efc8d791ffcb03e76f0936
A-lineed <aline.edvania.franca@outlook.com>
2023-02-24 18:02:45 -0300

    Fill in the mandatory fields and send the form

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,8 +1,20 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', function () {
-    it('verifica o título da aplicação', function () {
+    beforeEach(() => {
         cy.visit('./src/index.html')
-        cy.title().should('be.equal','Central de Atendimento ao Cliente TAT')
+    })
+    it('verifica o título da aplicação', function () {
+        cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    })
+
+    it('Preenche os campos obrigatórios e envia o formulário', () => {
+        cy.get('#firstName').type('Aline')
+        cy.get('#lastName').type('França')
+        cy.get('#email').type('aline@gmail.com')
+        cy.get('#open-text-area').type('Solicitação de ajuda')
+        cy.get('.button[type="submit"]').click()
+        cy.get('.success').should('be.visible')
+
     })
 })
