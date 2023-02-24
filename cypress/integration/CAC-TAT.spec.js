58df19ca22b6e0427c5a1ad4b93da4b15b2e8524
A-lineed <aline.edvania.franca@outlook.com>
2023-02-24 19:13:24 -0300

    using commands

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -68,9 +68,14 @@ describe('Central de Atendimento ao Cliente TAT', function () {
             .should('have.value', '')
     })
 
-    it.only('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios.', () => {
+    it('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios.', () => {
         cy.get('.button[type="submit"]').click()
         cy.get('.error').should('be.visible')
+    })
+
+    it.only('Envia o formuário com sucesso usando um comando customizado', () => {
+        cy.fillMandatoryFieldsAndSubmit('Aline', 'França', 'aline@gmail.com', 'Teste')
+        cy.get('.success').should('be.visible')
     })
 
 })
