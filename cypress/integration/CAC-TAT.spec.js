ba1db76cd64b1bace105089e49c047e3d2cf8e86
A-lineed <aline.edvania.franca@outlook.com>
2023-02-27 11:21:03 -0300

    Selecting options in dropdown selection fields

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -77,7 +77,25 @@ describe('Central de Atendimento ao Cliente TAT', function () {
     it('Envia o formuário com sucesso usando um comando customizado', () => {
         cy.fillMandatoryFieldsAndSubmit('Aline', 'França', 'aline@gmail.com', 'Teste')
         cy.get('.success').should('be.visible')
-        
+
+    })
+
+    it('seleciona um produto (YouTube) por seu texto', () => {
+        cy.get('#product')
+            .select('YouTube')
+            .should('have.value', 'youtube')
+    })
+
+    it('seleciona um produto (Mentoria) por seu valor (value)', () => {
+        cy.get('#product')
+            .select('mentoria')
+            .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) por seu índice', () => {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
     })
 
 })
